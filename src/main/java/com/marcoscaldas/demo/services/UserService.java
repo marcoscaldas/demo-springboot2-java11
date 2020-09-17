2ff8e66bb302323c6d11417c7b24c50265e854e7
marcoscaldas <donarumo@hotmail.com>
2020-09-17 16:55:14 -0300

    Service layer, component registration

---
+++ src/main/java/com/marcoscaldas/demo/services/UserService.java
@@ -0,0 +1,26 @@
+package com.marcoscaldas.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcoscaldas.demo.entities.User;
+import com.marcoscaldas.demo.repositories.UserRepository;
+
+@Service   //registrar a classe como componente do Spring
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
