d2ffe7276f0d384d3195194095ddc610b856fffc
marcoscaldas <donarumo@hotmail.com>
2020-09-18 09:26:25 -0300

    Category

---
+++ src/main/java/com/marcoscaldas/demo/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.marcoscaldas.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcoscaldas.demo.entities.Category;
+import com.marcoscaldas.demo.repositories.CategoryRepository;
+
+@Service   //registrar a classe como componente do Spring
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
