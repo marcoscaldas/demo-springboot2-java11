9821d0670bdde1ce3b1a850f049b7e6ed93a5e52
marcoscaldas <donarumo@hotmail.com>
2020-09-18 09:58:07 -0300

    Product

--- src/main/java/com/marcoscaldas/demo/repositories/CategoryRepository.java
+++ src/main/java/com/marcoscaldas/demo/repositories/CategoryRepository.java
@@ -3,7 +3,7 @@ package com.marcoscaldas.demo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.marcoscaldas.demo.entities.Category;
-import com.marcoscaldas.demo.entities.User;
+
 
 public interface CategoryRepository extends JpaRepository<Category, Long>{
 
---
+++ src/main/java/com/marcoscaldas/demo/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.marcoscaldas.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcoscaldas.demo.entities.Product;
+import com.marcoscaldas.demo.repositories.ProductRepository;
+
+@Service   //registrar a classe como componente do Spring
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
